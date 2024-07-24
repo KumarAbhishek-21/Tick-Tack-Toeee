37e7adab80adb711272b53433d2b9a9e70517fc0
KumarAbhishek-21 <abhishekkumar81148@gmail.com>
2024-07-24 20:37:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Components/Card/Card"
+import './App.css'
+import Grid from "./Components/Grid/Grid";
+
+function App() {
+
+  return (
+    <>
+    <Grid numberOfCards={9}/>
+    </>
+  );
+}
+
+export default App;
